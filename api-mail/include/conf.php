9064741f1697959ebe08a3e426c7d9623118e92f
manish-gary <manishsharma.gary@gmail.com>
2026-02-03 10:26:16 +0530

    api mail remove
    api mail remove

--- api-mail/include/conf.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-$CompanyName = "Taiyarikaro";
-$WebsiteLink = "https://www.taiyarikaro.com/";
-$CompanyLogo = "https://www.taiyarikaro.com/_next/image?url=%2Fnext_images%2Flogo.png&w=64&q=75";
-$CompanyDomain = "taiyarikaro.com";
-
-?>
